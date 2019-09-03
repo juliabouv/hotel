7557c71ccab2b979963808be7ec7a109b7ac66f4
Julia Bouvier <juliabouv@gmail.com>
2019-09-03 16:05:56 -0700

    required files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
-# Add simplecov
+require 'simplecov'
+require 'date'
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
@@ -6,3 +7,6 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+require_relative '../lib/hotel_booker'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
