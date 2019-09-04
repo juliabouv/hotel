7a3e69b01a85636e87130b063fceac9bc94d9d20
Julia Bouvier <juliabouv@gmail.com>
2019-09-03 17:44:03 -0700

    Implemented list_reservations_by_date method. Tests pass

--- lib/hotel_booker.rb
+++ lib/hotel_booker.rb
@@ -10,30 +10,50 @@ module Hotel
     def list_rooms
       @rooms = []
       Hotel::Room::TOTAL_ROOMS.times do |index|
-          room = Hotel::Room.new(
-            room_number: index + 1,
-            room_cost: 200.00
-          )
-          @rooms << room
+        room = Hotel::Room.new(
+          room_number: index + 1,
+          room_cost: 200.00
+        )
+        @rooms << room
       end
       return @rooms
     end
+
+    def list_reservations_by_date(date)
+      # select reservations that are within the start and end date range
+      date = Date.parse(date)
+      return @reservations.select { |reservation| reservation.checkin_date <= date && reservation.checkout_date >= date }
+    end
     
-    def list_reservations
-      
+
+    def book_reservation(checkin_date, checkout_date)
+      reservation = Hotel::Reservation.new(
+        checkin_date: checkin_date,
+        checkout_date: checkout_date,
+        status: :IN_PROGRESS,
+        room: find_room(1)
+      )
+      reservation.switch_status
+      @reservations << reservation
+      return reservation
     end
 
-    def book_reservation(checkin_date, checkout_date, room)
+    def find_reservations_by_room(room_number)
+      # create array of reservations
+      return @rooms.select { |room| room.number == room_number }
+    end
+    
+    def find_room(room_number)
+      # grab instance of room by room number
+      return @rooms.select { |room| room.room_number == room_number }.first
     end
 
     def room_availability(date)
-    end
+      # use length of reservation with start and end times to check availability
 
-    def total_reserved_nights
     end
-
-    def total_cost
-    end
+    
+    
     
   end
 end
