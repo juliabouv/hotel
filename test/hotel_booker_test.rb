1a99933c05b11cd8b3281f03faa8347d0b4a305c
Julia Bouvier <juliabouv@gmail.com>
2019-09-05 13:39:57 -0700

    Moved ArgumentError Tests for bad date to HotelBooker. Bug fixed

--- test/hotel_booker_test.rb
+++ test/hotel_booker_test.rb
@@ -39,15 +39,48 @@ describe "HotelBooker" do
     end
   end
   
+  describe "#book_reservation" do
+    before do
+      @hotel_booker = Hotel::HotelBooker.new
+    end
+
+    it "adds the reservation to the @reservations array" do
+     @hotel_booker.book_reservation(Date.today + 4, Date.today + 8)
+    
+     expect(@hotel_booker.reservations.length).must_equal 1
+      
+    end
+
+    it "raises an ArgumentError if check-in date is before today" do
+      expect { 
+        @hotel_booker.book_reservation(Date.today - 1, Date.today)
+      }.must_raise ArgumentError
+      
+    end
+    
+    it "raises an ArgumentError if check-in date is after check-out date" do
+      expect { 
+        @hotel_booker.book_reservation(Date.today + 2, Date.today + 1)
+      }.must_raise ArgumentError
+      
+    end
+    
+    it "raises an ArgumentError if check-in date and check-out date are the same" do
+      expect { 
+        @hotel_booker.book_reservation(Date.today + 4, Date.today + 4)
+      }.must_raise ArgumentError
+      
+    end
+  end
+  
   describe "#list_reservations_by_date" do
     before do
       @hotel_booker = Hotel::HotelBooker.new
     end
     
     it "Returns array of reservations" do
-      @hotel_booker.book_reservation('2020-09-15', '2020-09-18')
-      p @hotel_booker.reservations
-      @reservations = @hotel_booker.list_reservations_by_date('2020-09-16')
+      @hotel_booker.book_reservation(Date.today + 2, Date.today + 5)
+      @reservations = @hotel_booker.list_reservations_by_date(Date.today + 3)
       
       @reservations.each do |reservation|
         expect(reservation).must_be_kind_of Hotel::Reservation
@@ -57,10 +90,11 @@ describe "HotelBooker" do
     end
     
     it "Returns reservations only for date specified" do
-      @hotel_booker.book_reservation('2020-09-15', '2020-09-18')
-      @hotel_booker.book_reservation('2020-09-10', '2020-09-12')
-      @reservations = @hotel_booker.list_reservations_by_date('2020-09-16')
+      @hotel_booker.book_reservation(Date.today + 2, Date.today + 5)
+      @hotel_booker.book_reservation(Date.today + 8, Date.today + 12)
+      @reservations = @hotel_booker.list_reservations_by_date(Date.today + 3)
       expect(@reservations.length).must_equal 1
     end
   end
+  
 end
