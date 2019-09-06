7d90fd6a010eb227a56bc26d7a53cbbf77135fbd
Julia Bouvier <juliabouv@gmail.com>
2019-09-06 12:05:46 -0700

    Added instances of reservation for each room to reservations array

--- lib/room.rb
+++ lib/room.rb
@@ -1,17 +1,18 @@
 module Hotel
   class Room
-    attr_reader :number, :room_cost, :reservations, :TOTAL_ROOMS
+    attr_reader :number, :room_cost, :reservations
     
     TOTAL_ROOMS = 20
     
-    def initialize(number:, room_cost: 200.00)
+    def initialize(number:, room_cost: 200.00, reservations: nil)
       @number = number
       @room_cost = room_cost
+      @reservations = reservations || []
     end
     
-    def room_availability
-      
+    def add_reservation(reservation)
+      @reservations << reservation
     end
-    
+
   end
 end
