67e013577dcb75b34b7f7f70756e6893d56ecaf4
Julia Bouvier <juliabouv@gmail.com>
2019-09-06 13:41:35 -0700

    Implemented available? method. Tests pass

--- lib/room.rb
+++ lib/room.rb
@@ -13,6 +13,16 @@ module Hotel
     def add_reservation(reservation)
       @reservations << reservation
     end
-
+    
+    def available?(start_date, end_date)
+      @reservations.each do |reservation|
+        if (start_date > reservation.checkin_date && end_date < reservation.checkout_date) || (start_date < reservation.checkin_date && end_date > reservation.checkout_date) || (start_date < reservation.checkout_date && end_date > reservation.checkout_date) || (start_date < reservation.checkin_date && end_date > reservation.checkin_date) || (start_date == reservation.checkin_date && end_date == reservation.checkout_date)
+          return false
+        else
+          return true
+        end
+      end
+    end
+    
   end
 end
