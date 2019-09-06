c34417dc7fe5407d6a3976d53b911c65a1364f7b
Julia Bouvier <juliabouv@gmail.com>
2019-09-06 14:47:36 -0700

    Implemented booking room by availability and raising errors for unavailable rooms in book_reservation. Tests pass

--- lib/hotel_booker.rb
+++ lib/hotel_booker.rb
@@ -26,16 +26,22 @@ module Hotel
       return @rooms
     end
     
+   
     def book_reservation(checkin_date, checkout_date, room_number: nil)
       # raise ArgumentError for bad dates
       raise ArgumentError.new "Checkout date must be after checkin date" unless checkout_date > checkin_date
       raise ArgumentError.new "Checkin date cannot be before today" if checkin_date < Date.today
       raise ArgumentError.new "Only overnight stays allowed!" if checkin_date == checkout_date
       
+      room_options = list_rooms_available(checkin_date, checkout_date)
+      
+       # I can get a reservation of a room for a given date range, and that room will not be part of any other reservation overlapping that date range
       if room_number
         chosen_room = find_room(room_number)
+        raise ArgumentError.new "That room is already booked for that date range" unless room_options.include?(chosen_room)
       else
-        chosen_room = find_room(1)
+        chosen_room = room_options.find { |room| room }
+        raise ArgumentError.new "There are no rooms available for that date range" if chosen_room == nil 
       end
       
       reservation = Hotel::Reservation.new(
@@ -70,10 +76,7 @@ module Hotel
     
     # I can view a list of rooms that are not reserved for a given date range, so that I can see all available rooms for that day
     def list_rooms_available(start_date, end_date)
-      available_rooms = @rooms.select { |room| room.available?(start_date, end_date) }
-      
-      # return nil if no available rooms
-      return available_rooms.empty? ? nil : available_rooms
+      return @rooms.select { |room| room.available?(start_date, end_date) }
     end
   end
 end
